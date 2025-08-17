2682eea3889e2ff4febb4f8ba0ce1bebc6b2a099
Robert Graves <robertjgraves@gmail.com>
2025-08-17 09:35:39 -0700

    Browsable API

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        queryset=get_user_model().objects.all(), slug_field="username"
+    )
+
     class Meta:
         fields = (
             "id",
